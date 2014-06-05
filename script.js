19c4ab63a61bf33671c18e97f20768cd1ae10f74
subeenjeon <powersubeen@naver.com>
2014-06-05 22:29:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -41,3 +41,11 @@ var showPlayer = function() {
 	$("#menu").fadeOut();
 	$("#player").fadeIn();
 }
+
+var setVideo = function() {
+	var song = $("ul.song .selected").attr("id”);
+	var seat = $("ul.seat .selected").attr("id”);
+	var vidName = "video/" + song + "-" + seat + ".mp4”;
+	$("source").attr("src", vidName);
+	$("video").load();
+}
