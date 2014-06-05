81fb6effe06bcccf4230b44a0691c758ae4b3fcb
subeenjeon <powersubeen@naver.com>
2014-06-05 21:51:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ $(document).ready(function(){
 
 $(document).ready(function(){
 	$("ul.song li").click(function(){
+	$(this).removeClass("selected");
 	$(this).addClass("selected");
-	$(this).removeClass();
 	});
 });
 
